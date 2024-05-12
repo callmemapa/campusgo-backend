8c92ea2f08adcbd5ba5854c489d1ba73d25136f8
mariapaulamosquerarengifo <maria.mosquera@grupokonecta.com>
2024-05-11 20:12:56 -0500

    :rocket: update swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -13,9 +13,23 @@ async function bootstrap() {
     .setTitle('CampusGo! API')
     .setDescription('API para sistema de transporte universitario.')
     .setVersion('1.0')
+    .addServer('http://localhost:3000/', 'Local environment')
+    .addServer('https://campusgo-backend-kappa.vercel.app/', 'Production')
     .build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    customSiteTitle: 'Backend Generator',
+    customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+    ],
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+    ],
+  });
 
   await app.listen(process.env.PORT || 3000);
 }
