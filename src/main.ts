a297acf077d80fc2a87d7a208452c2c4306643c7
mariapaulamosquerarengifo <maria.mosquera@grupokonecta.com>
2024-05-14 16:12:33 -0500

    :dizzy: add cors config

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,13 @@ import { FirebaseModule } from './firebase.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    maxAge: 86400,
+  });
 
   FirebaseModule.initializeApp();
 
