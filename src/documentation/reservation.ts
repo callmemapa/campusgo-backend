9dcf9d0e338309c8608e830f780d90877ff57624
Juan David Castro <juan.david.castro@correounivalle.edu.co>
2024-05-26 18:10:33 -0500

    ✨ Added function to update and obtain all reservations.

--- src/documentation/reservation.ts
+++ src/documentation/reservation.ts
@@ -102,3 +102,94 @@ export const docGetReservation = {
     },
   },
 };
+
+export const getAllReservationsResponse = {
+  status: 200,
+  description: 'Reservas recuperadas exitosamente.',
+  schema: {
+    type: 'object',
+    properties: {
+      statusCode: { type: 'number', example: 200 },
+      message: { type: 'string', example: 'Reservas recuperadas exitosamente.' },
+      data: {
+        type: 'array',
+        items: {
+          type: 'object',
+          properties: {
+            id: { type: 'string', example: 'Z9L3DjDJd9FRMPLhqTLn' },
+            data: {
+              type: 'object',
+              properties: {
+                id_passenger: { type: 'string', example: 'eJphRYmHNt3vwYCM4Q7a' },
+                id_route: { type: 'string', example: 'bTGQkP4Jt8WdG7ZvFZpL' },
+                payment_method: { type: 'string', example: 'tarjeta de crédito' },
+                pick_up_point: { type: 'string', example: 'Avenida Siempre Viva' },
+                reservation_status: { type: 'string', example: 'confirmada' },
+                fare: { type: 'number', example: 2500 },
+                seats_to_reserve: { type: 'number', example: 2 },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+};
+
+export const updateReservationBody = {
+  type: Object,
+  description: 'Datos para actualizar una reserva',
+  examples: {
+    example: {
+      value: {
+        id_passenger: 'eJphRYmHNt3vwYCM4Q7a',
+        id_route: 'bTGQkP4Jt8WdG7ZvFZpL',
+        payment_method: 'tarjeta de crédito',
+        pick_up_point: 'Avenida Siempre Viva',
+        reservation_status: 'confirmada',
+        fare: 2500,
+        seats_to_reserve: 2,
+      },
+      summary: 'Ejemplo de actualización de reserva',
+    },
+  },
+};
+
+export const updateReservationResponse = {
+  status: 200,
+  description: 'Reserva actualizada exitosamente.',
+  schema: {
+    type: 'object',
+    properties: {
+      statusCode: { type: 'number', example: 200 },
+      message: { type: 'string', example: 'Reserva actualizada exitosamente.' },
+      data: {
+        type: 'object',
+        properties: {
+          id_passenger: { type: 'string', example: 'eJphRYmHNt3vwYCM4Q7a' },
+          id_route: { type: 'string', example: 'bTGQkP4Jt8WdG7ZvFZpL' },
+          payment_method: { type: 'string', example: 'tarjeta de crédito' },
+          pick_up_point: { type: 'string', example: 'Avenida Siempre Viva' },
+          reservation_status: { type: 'string', example: 'confirmada' },
+          fare: { type: 'number', example: 2500 },
+          seats_to_reserve: { type: 'number', example: 2 },
+        },
+      },
+    },
+  },
+};
+
+export const updateReservationResponseFailed = {
+  status: 400,
+  description: 'Error al actualizar la reserva',
+  schema: {
+    type: 'object',
+    properties: {
+      statusCode: { type: 'number', example: 400 },
+      message: {
+        type: 'string',
+        example: 'Hubo un problema al actualizar la reserva',
+      },
+    },
+  },
+};
